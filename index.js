95df2fd53fc283ce527170e2bc037cb7c2b4bc99
Chris Ding <chrisding7@berkeley.edu>
2022-09-01 13:53:04 -0700

    commit tree eff177c6904267df95d831402a7bcc467f1f9153

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (array) {
+    return array.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function (array) {
+    return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(n) {
+    return function (fare) {
+        return fare * n;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+//console.log(fareDouble(4)); -> 8
+const fareTripler = createFareMultiplier(3);
+//console.log(fareTripler(4)); -> 12
+
+function selectDifferentDrivers(driverArray, fn) {
+    return fn(driverArray);
+}
